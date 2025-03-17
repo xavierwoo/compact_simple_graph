043d7a721be3700e4f42a438cc9f074ee4ab2af4
xavierwoo <xavier.woo@gmail.com>
2025-03-17 17:15:34 +0800

    add message to  test_get_v_id function

--- csg/core.cpp
+++ csg/core.cpp
@@ -4,9 +4,10 @@
 #include "core.h"
 #include <string>
 #include <cassert>
+#include <iostream>
 
 namespace csg{
-    using std::string;
+    using std::string, std::cout, std::endl;
 
     void test_get_v_id(){
         Graph<string> graph;
@@ -16,6 +17,8 @@ namespace csg{
         assert(id == 1);
         id = graph.get_v_id("A");
         assert(id == 0);
+
+        cout<<"get_v_id() works"<<endl;
     }
 
 
