25f2628cad6ce781eafeb1f69c6f958c8be216e0
xavierwoo <xavier.woo@gmail.com>
2025-03-17 17:33:51 +0800

    add add_di_edge function

--- csg/core.h
+++ csg/core.h
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <vector>
 #include <cstdint>
+#include <cassert>
 
 namespace csg{
     using std::vector, std::unordered_map;
@@ -16,7 +17,8 @@ namespace csg{
         unordered_map<K, U> vertices_name_id_map;
         vector<vector<U>> edge_lists;
 
-        auto get_v_id(const K &v) -> U;
+        auto get_v_id(const K&) -> U;
+        void add_di_edge(const K&, const K&);
     };
 
     template<typename K, typename U>
@@ -28,10 +30,20 @@ namespace csg{
 
         U id {(U)vertices_name_id_map.size()};
         vertices_name_id_map[v] = id;
+        edge_lists.emplace_back();
         return id;
     }
 
+    template<typename K, typename U>
+    void Graph<K, U>::add_di_edge(const K& source, const K& target) {
+        auto s_id {get_v_id(source)};
+        auto t_id {get_v_id(target)};
+
+        edge_lists[s_id].push_back(t_id);
+    }
+
     [[maybe_unused]]void it_works();
+    void test_get_v_id();
 }
 
 #endif //COMPACT_SIMPLE_GRAPH_CORE_H
