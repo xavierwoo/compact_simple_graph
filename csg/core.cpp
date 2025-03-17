351fee76591fef65bf8ba3e6ca17b5f716eb236b
xavierwoo <xavier.woo@gmail.com>
2025-03-17 17:13:31 +0800

    add get_v_id function

--- csg/core.cpp
+++ csg/core.cpp
@@ -2,3 +2,26 @@
 // Created by 吴 歆韵 on 2025/3/17.
 //
 #include "core.h"
+#include <string>
+#include <cassert>
+
+namespace csg{
+    using std::string;
+
+    void test_get_v_id(){
+        Graph<string> graph;
+        auto id {graph.get_v_id("A")};
+        assert(id == 0);
+        id = graph.get_v_id("B");
+        assert(id == 1);
+        id = graph.get_v_id("A");
+        assert(id == 0);
+    }
+
+
+    [[maybe_unused]]
+    void it_works() {
+        test_get_v_id();
+    }
+}
+
--- csg/core.h
+++ csg/core.h
@@ -7,15 +7,31 @@
 
 #include <unordered_map>
 #include <vector>
-#include <string>
+#include <cstdint>
 
 namespace csg{
-    using std::vector, std::unordered_map, std::string;
-    template<typename U>
-    struct graph{
-        unordered_map<string, U> vertices_name_id_map;
+    using std::vector, std::unordered_map;
+    template<typename K, typename U = uint32_t>
+    struct Graph{
+        unordered_map<K, U> vertices_name_id_map;
         vector<vector<U>> edge_lists;
+
+        auto get_v_id(const K &v) -> U;
     };
+
+    template<typename K, typename U>
+    auto Graph<K, U>::get_v_id(const K &v) -> U {
+        auto id_iter {vertices_name_id_map.find(v)};
+        if(id_iter != vertices_name_id_map.end()){
+            return id_iter->second;
+        }
+
+        U id {(U)vertices_name_id_map.size()};
+        vertices_name_id_map[v] = id;
+        return id;
+    }
+
+    [[maybe_unused]]void it_works();
 }
 
 #endif //COMPACT_SIMPLE_GRAPH_CORE_H
